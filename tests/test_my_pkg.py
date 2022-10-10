4d63c6ec814069a490ba3190f1b39c6dc6bf798c
Bin Le <bin.le.code@gmail.com>
2022-10-09 23:07:34 -0400

    first cut

---
+++ tests/test_my_pkg.py
@@ -0,0 +1,10 @@
+import sys
+# import os
+
+# add project root to sys path to resolve package modules
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append('..')
+
+from my_pkg import my_module as mm
+
+mm.echo('hello pkg')
